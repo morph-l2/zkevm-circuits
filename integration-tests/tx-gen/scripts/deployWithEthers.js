0677abd18493e63025e840816b0915959d9ce461
anylots <842567494@qq.com>
2024-02-28 15:47:33 +0800

    and l2geth

--- integration-tests/tx-gen/scripts/deployWithEthers.js
+++ integration-tests/tx-gen/scripts/deployWithEthers.js
@@ -9,7 +9,7 @@ async function main() {
     ///prepare deployer
     let privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
     let customHttpProvider = new ethers.providers.JsonRpcProvider(
-        "http://localhost:8545"
+        "http://localhost:6688"
     );
     const signer = new ethers.Wallet(privateKey, customHttpProvider);
     console.log("signer.address: " + signer.address);
