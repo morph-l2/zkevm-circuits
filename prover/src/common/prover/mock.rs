8a07f2404cbfda1e9d9fabe4c40857614eb36e9c
dongliangCai <18307130121@fudan.edu.cn>
2024-03-07 10:48:56 +0800

    disable mockprover

--- prover/src/common/prover/mock.rs
+++ prover/src/common/prover/mock.rs
@@ -4,7 +4,7 @@ use halo2_proofs::{dev::MockProver, halo2curves::bn256::Fr};
 use once_cell::sync::Lazy;
 use snark_verifier_sdk::CircuitExt;
 
-pub static MOCK_PROVE: Lazy<bool> = Lazy::new(|| read_env_var("MOCK_PROVE", true));
+pub static MOCK_PROVE: Lazy<bool> = Lazy::new(|| read_env_var("MOCK_PROVE", false));
 
 impl Prover {
     pub fn assert_if_mock_prover<C: CircuitExt<Fr>>(id: &str, degree: u32, circuit: &C) {
@@ -20,7 +20,6 @@ impl Prover {
         
         mock_prover.assert_satisfied_par();
 
-        assert_eq!(mock_prover.verify(), Ok(()));
         
         log::info!("Mock prove for {id} - END");
     }
