ce67cd95addad052a6f6e9c0579005085aedcc36
anylots <842567494@qq.com>
2024-04-09 15:28:49 +0800

    update l2_predeployed

--- bus-mapping/src/l2_predeployed.rs
+++ bus-mapping/src/l2_predeployed.rs
@@ -10,9 +10,9 @@ pub mod message_queue {
 
     /// address of L2MessageQueue predeploy
     pub static ADDRESS: LazyLock<Address> =
-        LazyLock::new(|| Address::from_str("0x5300000000000000000000000000000000000000").unwrap());
+        LazyLock::new(|| Address::from_str("0x5300000000000000000000000000000000000001").unwrap());
     /// the slot of withdraw root in L2MessageQueue
-    pub static WITHDRAW_TRIE_ROOT_SLOT: U256 = U256::zero();
+    pub static WITHDRAW_TRIE_ROOT_SLOT: LazyLock<U256> = LazyLock::new(|| U256::from(33));
 }
 
 /// Helper for L1GasPriceOracle contract
@@ -22,7 +22,7 @@ pub mod l1_gas_price_oracle {
 
     /// L1GasPriceOracle predeployed address
     pub static ADDRESS: LazyLock<Address> =
-        LazyLock::new(|| Address::from_str("0x5300000000000000000000000000000000000002").unwrap());
+        LazyLock::new(|| Address::from_str("0x530000000000000000000000000000000000000F").unwrap());
     /// L1 base fee slot in L1GasPriceOracle
     pub static BASE_FEE_SLOT: LazyLock<U256> = LazyLock::new(|| U256::from(1));
     /// L1 overhead slot in L1GasPriceOracle
