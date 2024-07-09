1042f535f2c589bc274722a3996b878da6e6abae
anylots <842567494@qq.com>
2024-07-09 11:38:45 +0800

    test

--- zkevm-circuits/src/evm_circuit/param.rs
+++ zkevm-circuits/src/evm_circuit/param.rs
@@ -7,7 +7,7 @@ use halo2_proofs::{
 use std::{collections::HashMap, sync::LazyLock};
 
 // Step dimension
-pub(crate) const STEP_WIDTH: usize = 156;
+pub(crate) const STEP_WIDTH: usize = 154;
 /// Step height
 pub const MAX_STEP_HEIGHT: usize = 21;
 /// The height of the state of a step, used by gates that connect two
@@ -16,7 +16,7 @@ pub const MAX_STEP_HEIGHT: usize = 21;
 pub(crate) const STEP_STATE_HEIGHT: usize = 1;
 
 /// Number of Advice Phase2 columns in the EVM circuit
-pub(crate) const N_PHASE2_COLUMNS: usize = 9;
+pub(crate) const N_PHASE2_COLUMNS: usize = 7;
 
 /// Number of Advice Phase1 columns in the EVM circuit
 pub(crate) const N_PHASE1_COLUMNS: usize =
@@ -25,7 +25,7 @@ pub(crate) const N_PHASE1_COLUMNS: usize =
 // Number of copy columns
 pub(crate) const N_COPY_COLUMNS: usize = 2;
 // Number of copy columns for phase2
-pub(crate) const N_PHASE2_COPY_COLUMNS: usize = 2;
+pub(crate) const N_PHASE2_COPY_COLUMNS: usize = 1;
 
 pub(crate) const N_BYTE_LOOKUPS: usize = 48;
 
