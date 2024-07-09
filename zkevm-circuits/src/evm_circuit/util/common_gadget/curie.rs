38076603de7843b096a302f601d5444458d977c7
anylots <842567494@qq.com>
2024-07-09 19:31:43 +0800

    is_morph_chain

--- zkevm-circuits/src/evm_circuit/util/common_gadget/curie.rs
+++ zkevm-circuits/src/evm_circuit/util/common_gadget/curie.rs
@@ -4,7 +4,7 @@ use crate::{
         table::{FixedTableTag, Lookup},
         util::{
             constraint_builder::{ConstrainBuilderCommon, EVMConstraintBuilder},
-            math_gadget::{BatchedIsZeroGadget, LtGadget},
+            math_gadget::{IsZeroGadget, LtGadget},
         },
     },
     table::BlockContextFieldTag,
@@ -24,7 +24,7 @@ use halo2_proofs::{
 pub(crate) struct CurieGadget<F> {
     chain_id: Cell<F>,
     /// Morph chains have non-zero curie hard fork block number
-    is_morph_chain: BatchedIsZeroGadget<F, 3>,
+    is_morph_chain: IsZeroGadget<F>,
     /// The block height at which curie hard fork happens
     curie_fork_block_num: Cell<F>,
     pub(crate) is_before_curie: LtGadget<F, 8>, // block num is u64
@@ -48,13 +48,11 @@ impl<F: Field> CurieGadget<F> {
         //         * (chain_id.expr() - MORPH_TESTNET_CHAIN_ID.expr())
         //         * (chain_id.expr() - MORPH_DEVNET_CHAIN_ID.expr()),
         // );
-        let is_morph_chain = BatchedIsZeroGadget::construct(
+        let is_morph_chain = IsZeroGadget::construct(
             cb,
-            [
-                chain_id.expr() - MORPH_MAINNET_CHAIN_ID.expr(),
-                chain_id.expr() - MORPH_TESTNET_CHAIN_ID.expr(),
-                chain_id.expr() - MORPH_DEVNET_CHAIN_ID.expr(),
-            ],
+            (chain_id.expr() - MORPH_MAINNET_CHAIN_ID.expr())
+                * (chain_id.expr() - MORPH_TESTNET_CHAIN_ID.expr())
+                * (chain_id.expr() - MORPH_DEVNET_CHAIN_ID.expr()),
         );
 
         // For Morph Networks (mainnet, testnet, devnet),
@@ -103,11 +101,9 @@ impl<F: Field> CurieGadget<F> {
         self.is_morph_chain.assign(
             region,
             offset,
-            [
-                F::from(chain_id) - F::from(MORPH_MAINNET_CHAIN_ID),
-                F::from(chain_id) - F::from(MORPH_TESTNET_CHAIN_ID),
-                F::from(chain_id) - F::from(MORPH_DEVNET_CHAIN_ID),
-            ],
+            (F::from(chain_id) - F::from(MORPH_MAINNET_CHAIN_ID))
+                * (F::from(chain_id) - F::from(MORPH_TESTNET_CHAIN_ID))
+                * (F::from(chain_id) - F::from(MORPH_DEVNET_CHAIN_ID)),
         )?;
         let curie_fork_block_num = if chain_id == MORPH_TESTNET_CHAIN_ID {
             0
